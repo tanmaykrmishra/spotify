99460f174ee18b580365ff0b0e41c86be25b1be7
Tanmay Mishra <tanmaykumarmishra5@gmail.com>
2024-06-21 10:52:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 function App() {
+  const {audioRef,track} = useContext(PlayerContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen bg-black'>
+        <div className='h-[90%] flex'>
+            <Sidebar />
+            <Display />
+        </div>
+        <Player/>
+        <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
